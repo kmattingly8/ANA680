b9992fc7e38b8780b3b8dbd4f7d12c700695fbaf
kmattingly8 <kirsten.mattingly@icloud.com>
2025-02-13 17:39:50 -0800

    Updated Flask App & pkl file

--- ANA680_wk2/app.py
+++ ANA680_wk2/app.py
@@ -5,8 +5,7 @@ import numpy as np
 # Initialize the Flask app
 app = Flask(__name__)
 
-# Load the trained model (ensure the pickle file is in your project 
-folder)
+# Load the trained model (ensure the pickle file is in your project folder)
 model = joblib.load("breast_cancer_model.pkl")
 
 @app.route('/')
@@ -16,14 +15,18 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the input values from the form
-    var_1 = float(request.form['var_1'])
-    var_2 = float(request.form['var_2'])
-    var_3 = float(request.form['var_3'])
-    var_4 = float(request.form['var_4'])
-    var_5 = float(request.form['var_5'])
+    var_1 = float(request.form['Clump_thickness'])
+    var_2 = float(request.form['Uniformity_of_cell_size'])
+    var_3 = float(request.form['Uniformity_of_cell_shape'])
+    var_4 = float(request.form['Marginal_adhesion'])
+    var_5 = float(request.form['Single_epithelial_cell_size'])
+    var_6 = float(request.form['Bare_nuclei'])
+    var_7 = float(request.form['Bland_chromatin'])
+    var_8 = float(request.form['Normal_nucleoli'])
+    var_9 = float(request.form['Mitoses'])
 
     # Create an array of input data
-    input_data = np.array([[var_1, var_2, var_3, var_4, var_5]])
+    input_data = np.array([[var_1, var_2, var_3, var_4, var_5, var_6, var_7, var_8, var_9]])
 
     # Make a prediction using the model
     prediction = model.predict(input_data)
@@ -35,4 +38,3 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
